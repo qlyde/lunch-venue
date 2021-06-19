db8efe8be6b3cda2c9525ec93ad40efdbb5a62dd
James Kroeger <jameskroeger123@gmail.com>
2021-06-19 23:22:57 +1000

    extension 3: enforce a timeout in case no consensus is reached on the lunch venue

--- contracts/LunchVenue_updated.sol
+++ contracts/LunchVenue_updated.sol
@@ -39,6 +39,16 @@ contract LunchVenue {
     address public manager; // Manager of lunch venues
     string public votedVenue = ""; // Where to have lunch
 
+    // ------------------------ EXTENSION 3 ------------------------
+    // `timeoutBlock` is the block number at which the voting process will timeout.
+    // A timeout is signified by a transition to the `Finished` state.
+    // The `votedVenue` will remain empty since the friends could not agree on a venue.
+    //
+    // The default `timeoutBlock` will be 280 blocks from the current block.
+    // With an average block time of 13 seconds the timeout will be approx. 60 minutes from contract creation.
+    // -------------------------------------------------------------
+    uint private timeoutBlock = block.number + 280;
+
     mapping (uint => Vote) private votes; // List of votes (vote no, Vote)
     mapping (uint => uint) private results; // List of vote counts (venue no, no of votes)
 
@@ -50,12 +60,39 @@ contract LunchVenue {
         manager = msg.sender; // Set contract creator as manager
     }
 
+    // ------------------------ EXTENSION 3 ------------------------
+    /// @notice Set the `timeoutBlock`
+    /// @param blockNumber Block for timeout to occur
+    function setTimeout(uint blockNumber) public restricted {
+        require(state != State.Finished, "Voting process has already finished");
+        require(blockNumber >= block.number, "Timeout block cannot be in the past");
+        timeoutBlock = blockNumber;
+    }
+
+    // ------------------------ EXTENSION 3 ------------------------
+    /// @notice Extend the `timeoutBlock`
+    /// @param nblocks Number of blocks to extend the timeout for
+    function extendTimeout(uint nblocks) public restricted {
+        require(state != State.Finished, "Voting process has already finished");
+        timeoutBlock += nblocks;
+    }
+
+    // ------------------------ EXTENSION 3 ------------------------
+    /// @notice Reduce the `timeoutBlock`
+    /// @param nblocks Number of blocks to reduce the timeout for
+    function reduceTimeout(uint nblocks) public restricted {
+        require(state != State.Finished, "Voting process has already finished");
+        require(timeoutBlock - nblocks >= block.number, "Timeout block cannot be in the past");
+        timeoutBlock -= nblocks;
+    }
+
     /// @notice Add a new lunch venue
     /// @dev To simplify the code duplication of venues is not checked
     /// @param name Name of the venue
     /// @return Number of lunch venues added so far
     function addVenue(string memory name)
         public
+        timeoutTransition // ----- EXTENSION 3 -----
         restricted
         // ----- EXTENSION 2 ----- Must be in `Planning` stage to `addVenue()`
         stateIs(State.Planning)
@@ -73,6 +110,7 @@ contract LunchVenue {
     /// @return Number of friends added so far
     function addFriend(address friendAddress, string memory name)
         public
+        timeoutTransition // ----- EXTENSION 3 -----
         restricted
         // ----- EXTENSION 2 ----- Must be in `Planning` stage to `addFriend()`
         stateIs(State.Planning)
@@ -86,9 +124,17 @@ contract LunchVenue {
         return numFriends;
     }
 
+    // ------------------------ EXTENSION 2 ------------------------
+    // Transition from `Voting` state to `Planning` state.
+    // -------------------------------------------------------------
     /// @notice Begin voting stage
     /// @dev Can only transition to `Voting` state from the `Planning` state
-    function startVoting() public restricted stateIs(State.Planning) {
+    function startVoting()
+        public
+        timeoutTransition // ----- EXTENSION 3 -----
+        restricted
+        stateIs(State.Planning)
+    {
         state = State.Voting;
     }
 
@@ -98,6 +144,7 @@ contract LunchVenue {
     function doVote(uint venue)
         public
         // ----- EXTENSION 2 ----- Must be in `Voting` stage to `doVote()`
+        timeoutTransition // ----- EXTENSION 3 -----
         stateIs(State.Voting)
         returns (bool validVote)
     {
@@ -176,6 +223,16 @@ contract LunchVenue {
     /// @param _state Is the voting process in this state?
     modifier stateIs(State _state) {
         require(state == _state, "Function cannot be called in this state");
+        _;
+    }
+
+    // ------------------------ EXTENSION 3 ------------------------
+    // A modifier to transition state to `Finished` if the `timeoutBlock` has been reached.
+    // This modifier must be mentioned first so that other modifiers account for potential state change.
+    // -------------------------------------------------------------
+    /// @notice Check if the voting process has timed out and end voting process if so
+    modifier timeoutTransition() {
+        if (block.number >= timeoutBlock) state = State.Finished;
         _;
     }
 
